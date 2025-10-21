b730a85ff2af8e2844c7943452b4abcea1eb7773
jackson <jacksonduardo6@gmail.com>
2025-10-21 17:33:58 -0300

    Adicionar arquivos de projeto.

---
+++ AppControleFinanceiro/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "AppControleFinanceiro")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "AppControleFinanceiro.Pages")]
