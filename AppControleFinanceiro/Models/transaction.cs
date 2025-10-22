a7054a7672dd0653d7bd061a21e1975ccc6ef910
jackson <jacksonduardo6@gmail.com>
2025-10-21 21:01:29 -0300

    Go to Project

---
+++ AppControleFinanceiro/Models/transaction.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppControleFinanceiro.Models
+{
+    public class Transaction
+    {
+        public required int Id { get; set; }
+
+        public TransactionType  Type { get; set; }
+        public String Name { get; set; }
+        public DateTimeOffset Date { get; set; }
+        public Decimal Value { get; set; }
+    }
+    public enum TransactionType
+    {
+        Icome,
+        Expenses
+    }
+
+}
