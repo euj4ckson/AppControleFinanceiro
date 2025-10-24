ed0664d97abb9728cccecfab01c84a95553324c6
jackson <jacksonduardo6@gmail.com>
2025-10-24 15:39:38 -0300

    Final files

---
+++ AppControleFinanceiro/Libraries/Utils/FixBugs/KeyboardBugs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppControleFinanceiro.Libraries.Utils.FixBugs
+{
+    public class KeyboardBugs
+    {
+        public static void HideKeyboardOnAndroid()
+        {
+#if ANDROID
+            if (Platform.CurrentActivity is not null)
+            {
+                Platform.CurrentActivity.CurrentFocus.ClearFocus();
+            }
+#endif
+        }
+    }
+}
